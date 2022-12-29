8c144ef34243dfd3035c43d57994ca8dc447eebf
julianodibai <julianodibai2008@gmail.com>
2022-12-28 23:06:47 -0300

    Treinando jquery

---
+++ JqueryTraining/index.js
@@ -0,0 +1,16 @@
+
+//text
+$("#b5").text("Click $");
+$("button").hmtl("<em>click</em>");  //innerHtml do js vanila
+
+//styles
+$("h1").css("color", "red");
+$("h1").css("font-size", "100px");
+
+$("#t2").css("color", "aqua"); //id
+
+$("h3.t3").css("color", "blue"); //class
+
+$("h2").addClass("mudarCor mudarMargin"); //$("H2").removeClass("mudarCor");
+//evets
+
